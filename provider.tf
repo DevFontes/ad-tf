305479cc5d476b43753649c0f6ed978804a89b0c
Julio Fontes <julio.fontes@viewdeck.com>
2024-07-28 20:26:01 -0300

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    ad = {
+      source  = "hashicorp/ad"
+      version = "0.5.0"
+    }
+  }
+}
+
+provider "ad" {
+  winrm_hostname         = var.hostname
+  winrm_username         = var.username
+  winrm_password         = var.password
+#   winrm_use_ntlm = true
+  krb_realm              = var.domain
+  krb_conf               = "${path.module}/files/krb5.conf"
+  krb_spn                = "winserver1"
+  winrm_port             = 5986
+  winrm_proto            = "https"
+  winrm_pass_credentials = true
+  winrm_insecure         = true
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "username" {
+  default = "administrador"
+}
+
+variable "password" {
+  default = "orsi36621605@"
+}
+
+variable "hostname" {
+  default = "192.168.1.200"
+}
+
+variable "domain" {
+  default = "srvorsi.local"
+}
